620da2bed106eabaa31129237ca391eda93b42a1
Anirudh <anirudhbs96@gmail.com>
2018-01-17 16:42:20 +0530

    Add auth0 on backend

--- app.js
+++ app.js
@@ -2,16 +2,41 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const path = require('path')
+const jwt = require('express-jwt')
+const jwks = require('jwks-rsa')
 
 const app = express()
 const PORT = 8080
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(express.static('public'))
-app.use(bodyParser.json())
+
+const authCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://keep-up.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'http://learning-auth0.com',
+  issuer: 'https://keep-up.auth0.com/',
+  algorithms: ['RS256']
+})
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/list', authCheck, (req, res) => {
+  const list = [7, 8, 9]
+  res.json(list)
+})
+
+app.get('/otherlist', (req, res) => {
+  const list = [2001, 2002, 2003]
+  res.json(list)
 })
 
 app.listen(PORT, () => {
